59ecd66a29ce0411f41168df29bd3f22d4087bde
Gordon Beeming <gordon@devstarops.com>
2022-07-27 20:08:02 +0200

    rule not needed

--- scripts/start-vms.ps1
+++ scripts/start-vms.ps1
@@ -1,6 +1,12 @@
+az login
+az account set --subscription "8bec5d67-8e91-466a-8d6b-1bbfa4f642df"
+az account show
+
 # local
 az vm start --resource-group devstarops-local-rg --name local-dso-frontdoor-vm --no-wait
 az vm start --resource-group devstarops-local-rg --name local-dso-app1-vm --no-wait
 # test
 az vm start --resource-group devstarops-test-rg --name test-dso-frontdoor-vm --no-wait
 az vm start --resource-group devstarops-test-rg --name test-dso-app1-vm --no-wait
+
+. .\set-local-envs.ps1
--- scripts/stop-vms.ps1
+++ scripts/stop-vms.ps1
@@ -1,6 +1,12 @@
+az login
+az account set --subscription "8bec5d67-8e91-466a-8d6b-1bbfa4f642df"
+az account show
+
 # local
 az vm stop --resource-group devstarops-local-rg --name local-dso-frontdoor-vm --no-wait
 az vm stop --resource-group devstarops-local-rg --name local-dso-app1-vm --no-wait
 # test
 az vm stop --resource-group devstarops-test-rg --name test-dso-frontdoor-vm --no-wait
 az vm stop --resource-group devstarops-test-rg --name test-dso-app1-vm --no-wait
+
+. .\set-local-envs.ps1
